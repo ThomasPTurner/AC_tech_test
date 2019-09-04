c0cc9a3c01834d1d8407986db97e6e85432ce296
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-09-04 17:31:50 +0100

    wrote tests for international connection charge

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -14,5 +14,14 @@ describe('calculateCosts', () => {
     it('does not charge for incoming calls', ()=> {
         expect(utils.calculateCosts("0044123456,2019-09-04T12:42:01.636Z,01:00,INCOMING").cost).to.equal(0)
     })
+    it('charges 50p connection charge on international calls' , () => {
+        expect(utils.calculateCosts("0044123456,2019-09-04T12:42:01.636Z,00:00,OUTGOING").cost).to.equal(0.5)
+    })
+})
+
+describe('getOrigin', ()=> {
+    it('returns INTERNATIONAL for phone numbers beggining with 00', ()=> {
+        expect(utils.getOrigin("0044123456")).to.equal('INTERNATIONAL')
+    })
 })
 
